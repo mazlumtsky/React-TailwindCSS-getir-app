164c15639a3ebd87192ae6a06dda337227697478
Mazlum Taşkaya <104484087+mazlumtsky@users.noreply.github.com>
2022-08-22 18:23:22 +0300

    .

--- src/ui/ProdauctItems.js
+++ src/ui/ProdauctItems.js
@@ -3,7 +3,8 @@ import {HiPlus} from 'react-icons/hi'
 export default function ProdauctItems({ product }) {
     return (
         <div className='bg-white flex-col items-center relative text-center text-sm font-semibold  p-3 s'>
-            <button className='absolute top-2 right-3 bg-white w-8 h-8 flex items-center text-primary-brand-color justify-center border border-gray-200 rounded-lg shadow-lg  transition-all   hover:border-primary-brand-color'>
+            <button className='absolute top-2 right-3 bg-white w-8 h-8 flex items-center text-primary-brand-color 
+            justify-center border border-gray-200 rounded-lg shadow-lg  transition-all   hover:border-primary-brand-color'>
                 <HiPlus/>
             </button>
             <div className='hover:shadow transition-all hover:touch-pinch-zoom'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
         'primary-brand-color': '#5d3ebc',
         'secondary-brand-color': '#7849f7',
         'barand-yellow': '#ffd300'
+      }),
+      backgroundImage:theme =>({
+        'mobile-app':'url(https://getir.com/_next/static/images/doodle-d659f9f1fd505c811c2331fe3ffddd5f.png)'
       })
 
     },
